a310a0c22f00e7e3db016dd1d26b37dfe4c3ffe9
HaniAbiy <hanna.abiy@yahoo.com>
2024-11-27 20:31:03 -0500

    re-deploy

--- Netflix-clone/vite.config.js
+++ Netflix-clone/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/Netfix-clone-2024/',
-})
+  base: "/Netflix-clone-2024/",
+});
